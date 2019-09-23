06a5885a7527c5969fd2abfb889d489b11bcf59e
Steven Black <genseb7@gmail.com>
2019-09-23 15:54:36 -0400

    delete old script

--- az-devops-pipeline.sh
+++ az-devops-pipeline.sh
@@ -2,15 +2,6 @@
 
 PIPELINE=$1
 
-# faketty () {
-#   if [ "$(uname -s)" == "Darwin" ]
-#   then
-#     script -qF "$(printf "%q " "$@")"
-#   else
-#     script -qfec "$(printf "%q " "$@")" /dev/null
-#   fi
-# }
-
 ENDPOINT="$(az devops service-endpoint list | jq -r --arg name "${PIPELINE}-endpoint" '.[] | select(.name == $name) | .id')"
 
 if [ -z "${ENDPOINT}" ]
